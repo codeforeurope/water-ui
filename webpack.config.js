da6ca8574dc98febc174bd70c5a04558e880baf8
Duncan Healy <duncan.healy@gmail.com>
2017-03-30 17:45:44 +0100

    webpack different setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,148 @@
 var path = require('path')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
-var config = {
-  entry: path.resolve(__dirname, 'src/server.js'),
+var webpack = require('webpack')
+var postCssImport = require('postcss-import')
+var postCssNested = require('postcss-nested')
+var postCssSimpleVars = require('postcss-simple-vars')
+var postCssAutoprefixer = require('autoprefixer')
+process.traceDeprecation = true
+// TODO https://www.npmjs.com/package/transifex-loader
+
+module.exports = {
+  entry: {
+    'desktop/index': './js/desktop/index.js',
+    'desktop/post': './js/desktop/post.js',
+    'desktop/vendor': ['jquery'],
+
+    'mobile/index': './js/mobile/index.js',
+    'mobile/vendor': ['jquery']
+  },
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
+    filename: '[name].js?[hash]-[chunkhash]',
+    chunkFilename: '[name].js?[hash]-[chunkhash]',
+    path: path.join(__dirname, '/build'),
+    publicPath: '/build/'
   },
   module: {
     loaders: [
       {
+        test: /\.woff$/i,
+        loader: 'file-loader'
+      }, {
+        test: /\.eot$/i,
+        loader: 'file-loader'
+      }, {
+        test: /\.ttf$/,
+        loader: 'ttf-base64-loader',
+        exclude: /node_modules/
+      }, {
         test: /\.jsx?$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
         query: {
-          presets: ['es2015', 'react']
+          presets: [
+            'react',
+            [
+              'es2015', {
+                'modules': false
+              }
+            ],
+            'stage-0'
+          ]
         }
-      },
-      // Extract css files
-      {
+      }, {
+        test: /\.svg$/i,
+        loader: 'inline-loader'
+        /*     test: /\.svg$/,
+        loaders: [
+          'babel-loader', {
+            loader: 'react-svg',
+            query: {
+              svgo: {
+                plugins: [
+                  {
+                    removeTitle: false
+                  }
+                ],
+                floatPrecision: 2
+              }
+            }
+          }
+        ]
+*/
+      }, {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
-
-      },
-      // Optionally extract less files or any other compile-to-css language
-      {
-        test: /\.less$/,
-        loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader!less-loader' })
-
+        exclude: [
+          /node_modules/, /\.base\.css$/
+        ],
+        loader: ExtractTextPlugin.extract({
+          fallback: 'style',
+          use: [
+            'css?sourceMap&&modules&&importLoaders=1&localIdentName=[name]---[local]---[hash:' +
+                'base64:5]',
+            'postcss'
+          ]
+        })
+        /*
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          publicPath: '../'
+        })
+        */
+      }, {
+        test: /\.png$/,
+        loader: 'file-loader'
       }
     ]
-  }
-  plugins:[
-    ExtractTextPlugin
+  },
+  resolve: {
+    modules: [
+      'src', 'node_modules', 'web_modules'
+    ],
+    extensions: ['', '.json', '.js', '.jsx']
+  },
+  devtool: 'source-map',
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: function () {
+          return {
+            defaults: [
+              postCssImport, postCssNested, postCssSimpleVars, postCssAutoprefixer
+            ],
+            base: [postCssImport, postCssNested, postCssSimpleVars, postCssAutoprefixer]
+          }
+        }
+      }
+    }),
+    new ExtractTextPlugin({filename: 'css/[name].css?[hash]-[chunkhash]-[contenthash]-[name]', disable: false, allChunks: true}),
+    new webpack
+      .optimize
+      .CommonsChunkPlugin({
+        names: [
+          'desktop/common', 'desktop/vendor'
+        ],
+        chunks: [
+          'desktop/common', 'desktop/index', 'desktop/post'
+        ],
+        minChunks: 2
+      }),
+    new webpack
+      .optimize
+      .CommonsChunkPlugin({
+        names: [
+          'mobile/common', 'mobile/vendor'
+        ],
+        chunks: [
+          'mobile/common', 'mobile/index'
+        ],
+        minChunks: 2
+      })
   ]
 }
-
-module.exports = config
